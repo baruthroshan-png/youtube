54be186e44865441df132f6a671abc4aee614485
baruthroshan-png <baruthroshan@gmail.com>
2026-05-29 10:22:46 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+let count = 0;
+let target = 1000000;
+let speed = 10000;
+
+let counter = setInterval(() => {
+    count += speed;
+    document.getElementById("subs").innerText = count.toLocaleString() + "+";
+
+    if (count >= target) {
+        clearInterval(counter);
+        document.getElementById("subs").innerText = "1M+";
+    }
+}, 30);
